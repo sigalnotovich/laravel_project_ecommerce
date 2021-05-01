8a6ff9818fdb2c2a08ea78ac581693e1a524cf68
sigali <sigals.notov@gmail.com>
2021-05-01 14:07:18 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,16 @@ Route::get('/', function () {  //get fun retrive the home page of the app and re
     return view('welcome'); //welcome.blade.php
 });
 
-//Route::get('about',function(){ //when the user go the /about page the app opens the about.blade.php view
-//   return view('about') ;
-//});
-
-//Route::get('todos','App\Http\Controllers\ToDosController@index'); //when the user visites the about page laravel whould load the AboutControler and it will exceute the index function in this page
 
 Route::get('/about' ,function (){
     return view('about'); //about.blade.php
 //    echo "this is about page";
+})->middleware('check'); //use a middlware
+
+Route::get('/home' ,function (){
+    echo "this is home page";
 });
 
-//Route::get('/contact' ,function (){ //contact.blade.php
-//    return view('contact');
-////    echo "this is contact page";
-//});
 
-//when open the contact web page show the context of the index function
-//in the ContactController page
 //Route::get('/contact','/http/ContactController@index');//laravel7
 Route::get('/contact',[ContactController::class ,'index']);//laravel8
